e0bc6355a3bd3ae482debb556b41c4774d136c2c
Victor Vazquez <victor.vamv17@gmail.com>
2023-06-26 19:34:30 -0600

    add: Usando formatos de imagenes, usando la etiqueta video, configurando snipet

--- Proyecto4/gulpfile.js
+++ Proyecto4/gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
 function css(done){
     src('src/scss/**/*.scss')//Identificar el archivo de SASS
         .pipe( plumber())
@@ -9,10 +18,44 @@ function css(done){
 
     done();
 }
-function dev(done){
-    watch('src/scss/**/*.scss', css);
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones) ) )
+        .pipe( dest('build/img') )
 
     done();
 }
+
+function versionWebp(done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionAvif(done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function dev(done){
+    watch('src/scss/**/*.scss', css);
+    done();
+}
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel( imagenes,versionWebp, versionAvif, dev);
